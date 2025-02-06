363186bbadb69a7fa815797ccc714a911a9eef90
aybeedee <abdullahumer575@gmail.com>
2025-02-07 02:32:25 +0500

    chore: use api url from envar

--- frontend/src/api/task.ts
+++ frontend/src/api/task.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { Task } from "../types/task";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 export const TasksApi = {
